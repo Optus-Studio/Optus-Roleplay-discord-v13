b43c4152f69546fc550e73d557f800894e39803c
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-04-03 20:00:52 +0200

    fivem server status update

--- cmd/addons/join.js
+++ cmd/addons/join.js
@@ -4,36 +4,54 @@ const fs = require("fs");
 const ip = JSON.parse(fs.readFileSync("./src/addons/fivem-ip.json", "utf-8"));
 const server = new fivem.DiscordFivemApi(`${ip.server_ip}:${ip.server_port}`);
 
-
 module.exports.run = async (client, message, args) => {
 
+
+    server.getPlayers().then((data) => {
+        let result = [];
+        let index = 1;
+        for (let player of data) {
+            result.push(`${index++}. ${player.name} | ${player.id} ID | ${player.ping} ping\n`);
+        }
+        const playersOnline = await server.getPlayersOnline()
+    })
+
+
+
     var botEmbed = new discord.MessageEmbed()
-        .setTitle("Join Optus Roleplay")
-        .setDescription("Hoe Join Je **Optus Roleplay**")
+        .setTitle(`Players (${data.length}/${playersOnline})`)
+        .setDescription(result.length > 0 ? result : 'No Players Online!')
+        .setAuthor("Server is online")
         .setColor(process.env.COLLOR)
         .setThumbnail(process.env.LOGO)
         .setImage(process.env.BANNER)
         .setTimestamp()
         .setFooter("Join Optus Roleplay")
         .addFields(
-            { name: "**Stap 1**", value: "Start **FiveM**" },
-            { name: "**Stap 2**", value: "Druk Op **F8**" },
-            { name: "**Stap 3**", value: "Type `connect play.optusrp.nl`" },
-            { name: "*Hint*", value: "*je kan ook de knop hier onder indruken!*"},
-            { name: "**Stap 4**", value: "Veel Suc6 In **Optus Roleplay**"}
+            { name: "**S**", value: "Start **FiveM**" },
+            { name: "*Hint*", value: "*je kan ook de knop hier onder indruken!*" },
         )
-        
-        const row = new discord.MessageActionRow().addComponents(
 
-            new discord.MessageButton()
+    const row = new discord.MessageActionRow().addComponents(
+
+        new discord.MessageButton()
             .setLabel("Join Gelijk")
             .setStyle("LINK")
             .setEmoji("🎮")
             .setURL("https://cfx.re/join/lxz7q4")
-    
-        );
 
-    return message.channel.send({ embeds: [botEmbed], components: [row] }).then(msg => {
+    );
+
+    return message.channel.send({ embeds: [botEmbed], components: [row] }).catch((err) => {
+        var errEmbed = new discord.MessageEmbed()
+            .setColor("RED")
+            .setAuthor("Server is offline")
+            .setThumbnail(process.env.LOGO)
+            .setImage(process.env.BANNER)
+            .setTimestamp()
+            .setFooter("Join Optus Roleplay")
+
+    }).then(msg => {
         message.delete()
         setTimeout(() => msg.delete(), 20000);
     });
