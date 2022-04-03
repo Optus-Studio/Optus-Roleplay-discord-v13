81addea77dbe0e6bdfe18a029f4eadeb3d19248b
L3G3CLAN <81651393+L3G3CLAN@users.noreply.github.com>
2022-04-03 20:23:30 +0200

    Update join.js

--- cmd/addons/join.js
+++ cmd/addons/join.js
@@ -3,6 +3,7 @@ const fivem = require("discord-fivem-api");
 const fs = require("fs");
 const ip = JSON.parse(fs.readFileSync("./src/addons/fivem-ip.json", "utf-8"));
 const server = new fivem.DiscordFivemApi(`${ip.server_ip}:${ip.server_port}`);
+const playersOnline = await server.getPlayersOnline()
 
 module.exports.run = async (client, message, args) => {
 
@@ -12,7 +13,6 @@ module.exports.run = async (client, message, args) => {
         for (let player of data) {
             result.push(`${index++}. ${player.name} | ${player.id} ID | ${player.ping} ping\n`);
         }
-        var playersOnline = await server.getPlayersOnline()
     })
 
 
